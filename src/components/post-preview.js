a141f64b4f1d4cb30b08908f0834803d2393740e
gortron <gordon.lanza@gmail.com>
2020-06-10 14:46:29 -0700

    styling on post-preview

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -6,7 +6,18 @@ import styled from "@emotion/styled"
 import { useBreakpoint } from "gatsby-plugin-breakpoints"
 
 const StyledImgDesktop = styled(Img)`
+  width: 33vw;
+  margin: 2vw; 2vw 0 2vw;
+  // height: 33vw;
+  // object-fit: cover;
+`
+
+const StyledTitleBox = styled.div`
+  padding-right: 2vw;
   width: 37vw;
+  height: 4rem;
+  color: #002626;
+  text-align: right;
 `
 
 const StyledImgMobile = styled(Img)`
@@ -14,31 +25,48 @@ const StyledImgMobile = styled(Img)`
 `
 
 const Wrapper = styled.figure`
+  text-decoration: none;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 4vw;
+  background-color: #efe7da;
 `
 
 const WrappedImg = props => {
-  const { slug, fluid, alt } = { ...props }
+  const { post, fluid, alt } = { ...props }
+  const { title, slug, image, tags } = post.frontmatter
   const breakpoints = useBreakpoint()
+
+  let tags_string
+  if (tags) {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + ", " + currentValue
+    tags_string = tags.reduce(reducer)
+  }
+
   return (
     <Wrapper>
-      <Link to={slug}>
-        {breakpoints.sm ? (
-          <StyledImgMobile fluid={fluid} alt={alt} />
-        ) : (
-          <StyledImgDesktop fluid={fluid} alt={alt} />
-        )}
-      </Link>
+      {breakpoints.sm ? (
+        <StyledImgMobile fluid={fluid} alt={alt} />
+      ) : (
+        <div>
+          <Link to={slug}>
+            <StyledImgDesktop fluid={fluid} alt={alt} />
+          </Link>
+          <StyledTitleBox>
+            {tags && <i>{tags_string}</i>}
+            <h1>{title}</h1>
+          </StyledTitleBox>
+        </div>
+      )}
     </Wrapper>
   )
 }
 
 const PostPreview = ({ post }) => {
   const { title, slug, image } = post.frontmatter
-  return <WrappedImg fluid={image.sharp.fluid} alt={title} slug={slug} />
+  return <WrappedImg fluid={image.sharp.fluid} alt={title} post={post} />
 }
 
 export default PostPreview
